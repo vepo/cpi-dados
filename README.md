40e3f4e43f2af764627205a26ec11187140f87b9
Victor Osorio <victor.osorio@openet.com>
2021-07-13 22:08:08 -0300

    Busca com multiplos termos

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
-# cpi-dados
+# CPI Dados
+
+
+## Changelog
+
+0.0.1 - Versão inicial. Download e busca simples
+0.0.2 - Adicionando Lucene e busca indexada
+
--- src/main/java/br/com/povo/cpi/dados/DadosCpi.java
+++ src/main/java/br/com/povo/cpi/dados/DadosCpi.java
@@ -24,8 +24,11 @@ import org.apache.lucene.index.FilterDirectoryReader;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
 import org.apache.lucene.index.Term;
+import org.apache.lucene.search.BooleanClause.Occur;
+import org.apache.lucene.search.BooleanQuery;
 import org.apache.lucene.search.FuzzyQuery;
 import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
 import org.apache.lucene.search.ScoreDoc;
 import org.apache.lucene.store.FSDirectory;
 import org.jsoup.Jsoup;
@@ -43,52 +46,25 @@ public class DadosCpi {
         System.setProperty("org.apache.commons.logging.Log", "org.apache.commons.logging.impl.NoOpLog");
     }
 
-    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
+    private static void download(Element element, URL docUrl)
+            throws MalformedURLException, URISyntaxException, IOException, FileNotFoundException, InterruptedException {
+        var paths = docUrl.getPath().split("/");
+        var docData = biblioteca.resolve(paths[paths.length - 1] + ".pdf");
+        var docInfo = biblioteca.resolve(paths[paths.length - 1] + ".info");
+        if (!docData.toFile().exists()) {
+            System.out.println("Executing request: " + docUrl);
 
-        if (!biblioteca.toFile().exists()) {
-            biblioteca.toFile().mkdirs();
-        }
-
-        if (!luceneIndexPath.toFile().exists()) {
-            luceneIndexPath.toFile().mkdirs();
-        }
-
-        Document doc = Jsoup.connect("https://legis.senado.leg.br/comissoes/docsRecCPI?codcol=2441").get();
-        System.out.println(doc.title());
-        for (var element : doc.getElementsByTag("a")) {
-            if (element.hasAttr("href") && element.attr("href").contains("sdleg-getter")
-                    && element.text().startsWith("DOC ")) {
-                var docUrl = new URL(element.attr("href"));
-                download(element, docUrl);
-            }
-
-        }
-
-        indexer(luceneIndexPath);
-
-        var indexSearcher = new IndexSearcher(FilterDirectoryReader.open(FSDirectory.open(luceneIndexPath)));
-        try (var scanner = new Scanner(System.in)) {
-            String input = "";
-            while (!"sair".equalsIgnoreCase(input)) {
-                System.out.println(
-                        "Insira o termo de busca, ou \"sair\" para finalizar. Para encerrar a busca \"CTRL + C\"");
-                input = scanner.nextLine();
-                var termo = input;
-                var docs = indexSearcher.search(new FuzzyQuery(new Term("contents", termo)), 50);
-                if (docs.totalHits.value > 0l) {
-                    System.out.println("Termo encontrado em arquivos " + docs.totalHits.value);
-                    for (ScoreDoc score : docs.scoreDocs) {
-                        System.out.printf("%f - %s\n", score.score,
-                                indexSearcher.doc(score.doc).getField("filename").stringValue());
-                    }
-
-                } else {
-                    System.out.println("Termo não encontrado!");
+            var request = HttpRequest.newBuilder().uri(docUrl.toURI()).build();
+            var response = client.send(request, BodyHandlers.ofInputStream());
+            if (response.statusCode() == 200) {
+                try (var writer = new FileOutputStream(docData.toFile());
+                        var infoWriter = new BufferedWriter(new FileWriter(docInfo.toFile()))) {
+                    infoWriter.write(element.text());
+                    writer.write(response.body().readAllBytes());
                 }
+                System.out.println("Arquivo salvo: " + element.text());
             }
         }
-        System.out.println("Busca encerada!");
-
     }
 
     public static void indexer(Path indexDirectoryPath) throws IOException {
@@ -119,23 +95,86 @@ public class DadosCpi {
         }
     }
 
-    private static void download(Element element, URL docUrl)
-            throws MalformedURLException, URISyntaxException, IOException, FileNotFoundException, InterruptedException {
-        var paths = docUrl.getPath().split("/");
-        var docData = biblioteca.resolve(paths[paths.length - 1] + ".pdf");
-        var docInfo = biblioteca.resolve(paths[paths.length - 1] + ".info");
-        if (!docData.toFile().exists()) {
-            System.out.println("Executing request: " + docUrl);
+    public static void main(String[] args) throws IOException, URISyntaxException, InterruptedException {
 
-            var request = HttpRequest.newBuilder().uri(docUrl.toURI()).build();
-            var response = client.send(request, BodyHandlers.ofInputStream());
-            if (response.statusCode() == 200) {
-                try (var writer = new FileOutputStream(docData.toFile());
-                        var infoWriter = new BufferedWriter(new FileWriter(docInfo.toFile()))) {
-                    infoWriter.write(element.text());
-                    writer.write(response.body().readAllBytes());
+        if (!biblioteca.toFile().exists()) {
+            biblioteca.toFile().mkdirs();
+        }
+
+        if (!luceneIndexPath.toFile().exists()) {
+            luceneIndexPath.toFile().mkdirs();
+        }
+
+        Document doc = Jsoup.connect("https://legis.senado.leg.br/comissoes/docsRecCPI?codcol=2441").get();
+        System.out.println(doc.title());
+        for (var element : doc.getElementsByTag("a")) {
+            if (element.hasAttr("href") && element.attr("href").contains("sdleg-getter")
+                    && element.text().startsWith("DOC ")) {
+                var docUrl = new URL(element.attr("href"));
+                download(element, docUrl);
+            }
+
+        }
+
+        indexer(luceneIndexPath);
+
+        realizaBusca();
+        System.out.println("Busca encerada!");
+
+    }
+
+    private static void realizaBusca() throws IOException {
+        var indexSearcher = new IndexSearcher(FilterDirectoryReader.open(FSDirectory.open(luceneIndexPath)));
+        Query ultimaQuery = null;
+        ScoreDoc ultimoScore = null;
+        int offset = 0;
+        try (var scanner = new Scanner(System.in)) {
+            String input = "";
+            while (!"sair".equalsIgnoreCase(input)) {
+                if (null != ultimaQuery && null != ultimoScore) {
+                    System.out.println(
+                            "Insira o termo de busca, \"mais\" para exibir mais resultados da última busca, ou \"sair\" para finalizar. Para encerrar a busca \"CTRL + C\"");
+                } else {
+                    System.out.println(
+                            "Insira o termo de busca, ou \"sair\" para finalizar. Para encerrar a busca \"CTRL + C\"");
                 }
-                System.out.println("Arquivo salvo: " + element.text());
+                input = scanner.nextLine();
+                var termo = input;
+                if (null != ultimaQuery && null != ultimoScore && "mais".equalsIgnoreCase(termo)) {
+                    var docs = indexSearcher.searchAfter(ultimoScore, ultimaQuery, 50);
+                    if (docs.scoreDocs.length > 0l) {
+                        System.out.println("Termo encontrado em arquivos " + (offset + docs.scoreDocs.length) + "/"
+                                + docs.totalHits.value);
+                        for (ScoreDoc score : docs.scoreDocs) {
+                            System.out.printf("%f - %s\n", score.score,
+                                    indexSearcher.doc(score.doc).getField("filename").stringValue());
+                        }
+                        ultimoScore = docs.scoreDocs[docs.scoreDocs.length - 1];
+                        offset += docs.scoreDocs.length;
+                    } else {
+                        System.out.println("Termo não encontrado!");
+                        ultimoScore = null;
+                    }
+                } else {
+                    var queries = new BooleanQuery.Builder();
+                    Stream.of(termo.split(" ")).filter(s -> !s.isBlank()).forEachOrdered(
+                            palavra -> queries.add(new FuzzyQuery(new Term("contents", palavra)), Occur.MUST));
+                    var docs = indexSearcher.search(ultimaQuery = queries.build(), 50);
+                    offset = 0;
+                    if (docs.totalHits.value > 0l) {
+                        System.out.println(
+                                "Termo encontrado em arquivos " + docs.scoreDocs.length + "/" + docs.totalHits.value);
+                        for (ScoreDoc score : docs.scoreDocs) {
+                            System.out.printf("%f - %s\n", score.score,
+                                    indexSearcher.doc(score.doc).getField("filename").stringValue());
+                        }
+                        ultimoScore = docs.scoreDocs[docs.scoreDocs.length - 1];
+                        offset += docs.scoreDocs.length;
+                    } else {
+                        System.out.println("Termo não encontrado!");
+                        ultimoScore = null;
+                    }
+                }
             }
         }
     }
